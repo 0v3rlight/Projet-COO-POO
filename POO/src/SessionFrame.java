a115ff1f18a991c2f63691ade6859d28eda9f672
M. Vendeville Benjamin <vendevil@insa-08123>
2019-12-03 18:05:22 +0100

    history finished

---
+++ POO/src/SessionFrame.java
@@ -0,0 +1,23 @@
+import java.awt.event.ActionListener;
+import userGestion.*;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class SessionFrame implements ActionListener {
+    JFrame Frame;
+    JPanel ChatPanel;
+    JTextField History;
+    JButton SendButton;
+    Session CurrentSession;
+    
+    public SessionFrame(User DistantUser)
+    {
+    	
+    }
+	
+
+}
