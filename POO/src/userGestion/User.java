86149d2eb05b1c1ed8a848cafd3ffb30cf3d0767
M. Vendeville Benjamin <vendevil@insa-10577>
2020-01-15 17:30:04 +0100

    MAJ

--- POO/src/userGestion/User.java
+++ POO/src/userGestion/User.java
@@ -1,11 +1,10 @@
 package userGestion;
 
-import java.net.*;
-
 public class User {
 	
 	public String Pseudo;
 	public String IP_address;
+	protected int Port;
 	
 	public User(String Ps) {
 		this.Pseudo = Ps ;
@@ -16,8 +15,18 @@ public class User {
 		this.IP_address = IP ;
 	}
 	
+	public User(String Ps, String IP, int Port) {
+		this.Pseudo = Ps ;
+		this.IP_address = IP ;
+		this.Port = Port;
+	}
+	
 	public void SetPseudo(String ps) {
 		this.Pseudo = ps ;
+	}
+	
+	public int getUserPort() {
+		return this.Port;
 	}
 	
 	public String getUserPseudo() {
